293b7b818f576dd14dd55a8f762c06319fa2ed5f
Ben Blank <ben.blank@gmail.com>
2013-08-13 22:20:05 -0700

    cleanup, tweak lily pad growth

--- src/com/five35/minecraft/greenthumb/GreenThumb.java
+++ src/com/five35/minecraft/greenthumb/GreenThumb.java
@@ -1,11 +1,11 @@
 package com.five35.minecraft.greenthumb;
 
-import java.util.Random;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemDye;
@@ -17,7 +17,7 @@ import net.minecraftforge.event.Event.Result;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.player.BonemealEvent;
 
-@Mod(modid="GreenThumb")
+@Mod(modid = "GreenThumb")
 public class GreenThumb {
 	@Instance
 	public static GreenThumb instance;
@@ -25,7 +25,7 @@ public class GreenThumb {
 	static Configuration config;
 	static boolean easyMode;
 
-	private Random random = new Random();
+	private final Random random = new Random();
 
 	public static boolean applyBonemeal(final ItemStack stack, final World world, final int x, final int y, final int z, final EntityPlayer player) {
 		if (GreenThumb.easyMode) {
@@ -55,19 +55,19 @@ public class GreenThumb {
 		if (event.ID == Block.cactus.blockID || event.ID == Block.reed.blockID) {
 			int height = 1;
 			int y = event.Y;
-			
+
 			// count plant blocks below this one
 			while (event.world.getBlockId(event.X, --y, event.Z) == event.ID) {
 				height++;
 			}
-			
+
 			y = event.Y;
-			
+
 			// count plant blocks above this one, and also find the Y coordinate above the "plant stack"
 			while (event.world.getBlockId(event.X, ++y, event.Z) == event.ID) {
 				height++;
 			}
-			
+
 			if (height < 3 && event.world.isAirBlock(event.X, y, event.Z)) {
 				event.world.setBlock(event.X, y, event.Z, event.ID);
 				Block.blocksList[event.ID].onNeighborBlockChange(event.world, event.X, y, event.Z, event.ID);
@@ -77,14 +77,14 @@ public class GreenThumb {
 		} else if (event.ID == Block.waterlily.blockID) {
 			int count = 0;
 
-			// count the lily pads in a 9x9 square centered on this one
-			for (int dX = -4; dX < 5; dX++) {
-				for (int dZ = -4; dZ < 5; dZ++) {
+			// count the lily pads in a 7x7 square centered on this one
+			for (int dX = -3; dX < 4; dX++) {
+				for (int dZ = -3; dZ < 4; dZ++) {
 					if (event.world.getBlockId(event.X + dX, event.Y, event.Z + dZ) == Block.waterlily.blockID) {
 						count++;
 					}
 
-					if (count > 11) { // 12 lily pads in a 9x9 square ~= 15% coverage
+					if (count > 11) { // 12 lily pads in a 7x7 square ~= 25% coverage
 						return;
 					}
 				}
@@ -92,20 +92,22 @@ public class GreenThumb {
 
 			int attempts = 0;
 
-			// attempt to place a new lily pad in a 5x5 square centered on this one
-			while (true) {
-				int x = event.X + this.random.nextInt(5) - 2;
-				int z = event.Z + this.random.nextInt(5) - 2;
+			// attempt to place a new lily pad in a 7x7 square centered on this one
+			if (!event.world.isRemote) {
+				while (true) {
+					final int x = event.X + this.random.nextInt(7) - 3;
+					final int z = event.Z + this.random.nextInt(7) - 3;
 
-				if (event.world.isAirBlock(x, event.Y, z) && Block.waterlily.canBlockStay(event.world, x, event.Y, z)) {
-					event.world.setBlock(x, event.Y, z, Block.waterlily.blockID, 0, 2);
+					if (event.world.isAirBlock(x, event.Y, z) && Block.waterlily.canBlockStay(event.world, x, event.Y, z)) {
+						event.world.setBlock(x, event.Y, z, Block.waterlily.blockID, 0, 2);
 
-					break;
-				}
+						break;
+					}
 
-				// give up (and don't consume bonemeal) after 10 failed attempts at placement
-				if (++attempts > 9) {
-					return;
+					// give up (and don't consume bonemeal) after 10 failed attempts at placement
+					if (++attempts > 9) {
+						return;
+					}
 				}
 			}
 		} else {
