1da1bd17651da6314e4ccd24b2cc5a578514d213
Ben Blank <ben.blank@gmail.com>
2015-06-18 08:47:56 -0700

    add core mod

---
+++ src/main/java/com/five35/minecraft/greenthumb/LoadingPlugin.java
@@ -0,0 +1,35 @@
+package com.five35.minecraft.greenthumb;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin.Name;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
+import java.util.Map;
+
+@MCVersion("1.7.10")
+@Name("GreenThumb")
+@TransformerExclusions("com.five35.greenthumb.")
+public class LoadingPlugin implements IFMLLoadingPlugin {
+	@Override
+	public String getAccessTransformerClass() {
+		return null;
+	}
+
+	@Override
+	public String[] getASMTransformerClass() {
+		return null;
+	}
+
+	@Override
+	public String getModContainerClass() {
+		return null;
+	}
+
+	@Override
+	public String getSetupClass() {
+		return null;
+	}
+
+	@Override
+	public void injectData(final Map<String, Object> data) {}
+}
