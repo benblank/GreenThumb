68dad38d9a76eb9253408d54c4f79aa9471c41b6
Ben Blank <ben.blank@gmail.com>
2015-06-20 10:10:21 -0700

    update loading plugin for 1.8

--- src/main/java/com/five35/minecraft/greenthumb/LoadingPlugin.java
+++ src/main/java/com/five35/minecraft/greenthumb/LoadingPlugin.java
@@ -1,10 +1,10 @@
 package com.five35.minecraft.greenthumb;
 
-import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
-import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
-import cpw.mods.fml.relauncher.IFMLLoadingPlugin.Name;
-import cpw.mods.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
 import java.util.Map;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.MCVersion;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.Name;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
 
 @MCVersion("1.7.10")
 @Name("GreenThumb")
